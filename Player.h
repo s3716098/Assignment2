e6b732e9aa5f1c4154a850b817588e07aa985cc8
s3716098 <69687730+s3716098@users.noreply.github.com>
2021-05-16 14:18:23 +0900

    Player created

---
+++ Player.h
@@ -0,0 +1,29 @@
+#ifndef ASSIGN2_PLAYER_H
+#define ASSIGN2_PLAYER_H
+
+#include "Tile.h"
+#include "LinkedList.h"
+#include <string>
+using namespace std;
+
+class Player
+{
+public:
+    Player(string name, int Score, LinkedList *TilesInHand);
+
+    int getScore();
+    void setScore(int Score);
+
+    LinkedList *getTilesInHand();
+    void setTilesInHand(LinkedList *TilesInHand);
+
+    string getName();
+    void setName(string name);
+
+private:
+    string name;
+    int Score;
+    LinkedList *TilesInHand;
+};
+
+#endif // ASSIGN2_NODE_H
